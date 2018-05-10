2e4558434f52379caa354b0f050513bcc912fbb1
Savannah Pyle <bea12022@byui.edu>
2018-05-09 22:49:31 -0600

    testimage

--- web/shoppingmain.php
+++ web/shoppingmain.php
@@ -90,12 +90,12 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <div class="w3-row w3-grayscale">
     <div class="w3-col l3 s6">
       <div class="w3-container">
-        <img src="/w3images/jeans1.jpg" style="width:100%">
-        <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
+        <img src="estee1.png" style="width:100%">
+        <p>Ripped Skinny Jeans<br><b>$24.99</b><button type="button" class="btn">Basic</button></p>
       </div>
       <div class="w3-container">
         <img src="/w3images/jeans2.jpg" style="width:100%">
-        <p>Mega Ripped Jeans<br><b>$19.99</b></p>
+        <p>Mega Ripped Jeans<br><b>$19.99</b><button type="button" class="btn">Basic</button></p>
       </div>
     </div>
 
@@ -108,18 +108,18 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
             <button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button>
           </div>
         </div>
-        <p>Mega Ripped Jeans<br><b>$19.99</b> <button type="button" class="btn">Basic</button></p>
+        <p>Mega Ripped Jeans<br><b>$19.99</b><button type="button" class="btn">Basic</button></p>
       </div>
       <div class="w3-container">
         <img src="/w3images/jeans3.jpg" style="width:100%">
-        <p>Washed Skinny Jeans<br><b>$20.50</b></p>
+        <p>Washed Skinny Jeans<br><b>$20.50</b><button type="button" class="btn">Basic</button></p>
       </div>
     </div>
 
     <div class="w3-col l3 s6">
       <div class="w3-container">
         <img src="/w3images/jeans3.jpg" style="width:100%">
-        <p>Washed Skinny Jeans<br><b>$20.50</b></p>
+        <p>Washed Skinny Jeans<br><b>$20.50</b><button type="button" class="btn">Basic</button></p>
       </div>
       <div class="w3-container">
         <div class="w3-display-container">
@@ -129,18 +129,18 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
             <button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button>
           </div>
         </div>
-        <p>Vintage Skinny Jeans<br><b class="w3-text-red">$14.99</b></p>
+        <p>Vintage Skinny Jeans<br><b class="w3-text-red">$14.99</b><button type="button" class="btn">Basic</button></p>
       </div>
     </div>
 
     <div class="w3-col l3 s6">
       <div class="w3-container">
         <img src="/w3images/jeans4.jpg" style="width:100%">
-        <p>Vintage Skinny Jeans<br><b>$14.99</b></p>
+        <p>Vintage Skinny Jeans<br><b>$14.99</b<button type="button" class="btn">Basic</button></p>
       </div>
       <div class="w3-container">
         <img src="/w3images/jeans1.jpg" style="width:100%">
-        <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
+        <p>Ripped Skinny Jeans<br><b>$24.99</b><button type="button" class="btn">Basic</button></p>
       </div>
     </div>
   </div>
