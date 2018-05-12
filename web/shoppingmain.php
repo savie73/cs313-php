425c92d4aff77085cec6f24764595a2f3c9b8ff7
Savannah Pyle <bea12022@byui.edu>
2018-05-12 16:11:36 -0600

    stuff

--- web/shoppingmain.php
+++ web/shoppingmain.php
@@ -132,7 +132,7 @@ bodzy,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
         <p>Double Wear Nude Fresh<br><b><br><button type="button" class="btn" onclick="addToCart('Double Wear Nude Fresh')" >Add</button></p>
       </div>
     </div>
-
+    <div class="w3-col l3 s6">
       <div class="w3-container">
         <img src="estee7.PNG" style="width:100%">
         <p>Pure Color Envy Lip Balm<br><b><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy Lip Balm')" >Add</button></p>
@@ -160,7 +160,7 @@ bodzy,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
       <div class="w3-container">
         <img src="estee8.PNG" style="width:100%">
 
-        <p>Pure Color Envy Sculpting<br><b><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy Sculpting')">Add</button></p>
+        <p>Pure Color Envy Sculpting<br><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy Sculpting')">Add</button></p>
         
     </div>
   </div>
