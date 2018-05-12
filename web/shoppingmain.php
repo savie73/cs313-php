277f12e6c3a6845ef26950f42cf063840dcb6bd6
Savannah Pyle <bea12022@byui.edu>
2018-05-12 16:22:30 -0600

    stuff

--- web/shoppingmain.php
+++ web/shoppingmain.php
@@ -145,9 +145,8 @@ bodzy,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
         <p>Double Wear<br><br><button type="button" class="btn" onclick="addToCart('Double Wear')">Add</button></p>
               </div>
       <div class="w3-container">
-        <div class="w3-display-container">
+        <!-- <div class="w3-display-container"> -->
           <img src="estee6.PNG" style="width:100%">
-        </div>
         <p>Pure Color Envy<br><b><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy')">Add</button></p>
       </div>
     </div>
