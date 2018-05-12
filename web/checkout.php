5d11fedbbd714244c047bb4a36d63d7892b3449a
Savannah Pyle <bea12022@byui.edu>
2018-05-12 15:44:56 -0600

    stuff

---
+++ web/checkout.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+	<form action="confirmation.php" method="POST">
+		Name: <input type="text" name="name"><br>
+        E-Mail: <input type="text" name="email"><br>
+        Address: <input type="text" name="address"><br>
+        <input type="submit">
+	</form>
+</body>
+</html>
