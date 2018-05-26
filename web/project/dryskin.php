08e0824c1244f6bce071fbe21730e628c34bbfc1
Savannah Pyle <bea12022@byui.edu>
2018-05-26 15:35:05 -0600

    lego

--- web/project/dryskin.php
+++ web/project/dryskin.php
@@ -110,55 +110,7 @@ bodzy,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
  -->
   
   <!-- Product grid -->
-  <div class="w3-row">
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="estee1.PNG" style="width:100%">
-        <p>Perfectionist<br><br><button type="button" id="found1" class="btn" onclick="addToCart('Perfectionist')">Add</button></p>
-      </div>
-      <div class="w3-container">
-        <img src="estee5.PNG" style="width:100%">
-        <p>Limited Edition Pure Color<br><b><button type="button" id="lip1" class="btn" onclick="addToCart('Limited Edition Pure Color')">Add</button></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="estee2.PNG" style="width:100%">
-        </div>
-          <p>Double Wear Nude Fresh<br><b><br><button type="button" class="btn" onclick="addToCart('Double Wear Nude Fresh')" >Add</button></p>
-      </div>
-    
-      <div class="w3-container">
-        <img src="estee7.PNG" style="width:100%">
-        <p>Pure Color Envy Lip Balm<br><b><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy Lip Balm')" >Add</button></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="estee3.PNG" style="width:100%">
-        <p>Double Wear<br><br><button type="button" class="btn" onclick="addToCart('Double Wear')">Add</button></p>
-      </div>
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="estee6.PNG" style="width:100%">
-        </div>
-        <p>Pure Color Envy<br><b><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy')">Add</button></p>
-      </div>
-    </div>
-   
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="estee4.PNG" style="width:100%">
-        <p>Double Wear Light<br><br><button type="button" class="btn" onclick="addToCart('Double Wear Light')">Add</button></p>
-      </div>
-      <div class="w3-container">
-        <img src="estee8.PNG" style="width:100%">
-        <p>Pure Color Envy Sculpting<br><br><button type="button" class="btn" onclick="addToCart('Pure Color Envy Sculpting')">Add</button></p>
-    </div>
-  </div>
+  
 
    <!-- PRODUCT END 
   PRODUCT END 
