0360274890bae63ffb47ebb60aee53f085813f1d
Savannah Pyle <bea12022@byui.edu>
2018-06-02 12:33:28 -0600

    here

--- web/project/results.php
+++ web/project/results.php
@@ -34,7 +34,7 @@ catch (PDOException $ex)
 	{
 		$source = 20;
 		$stmt = $db->query("SELECT * FROM foundation WHERE price <= 20 AND skin_id = :skint");
-		$stmt->bindValue('skin_id', $skin_id, PDO::PARAM_INT);
+		$stmt->bindValue('skint', $skin_id, PDO::PARAM_INT);
 		$stmt->execute();
 		$rows = $stmt->fetchAll(PDO:: FETCH_ASSOC);
 		foreach ($rows as $row)
@@ -51,7 +51,7 @@ catch (PDOException $ex)
 	{
 		$source = 21;
 		$stmt = $db->query("SELECT * FROM foundation WHERE price >= 21 AND skin_id = :skint");
-		$stmt->bindValue('skin_id', $skin_id, PDO::PARAM_INT);
+		$stmt->bindValue('skint', $skin_id, PDO::PARAM_INT);
 		$stmt->execute();
 		$rows = $stmt->fetchAll(PDO:: FETCH_ASSOC);
 		foreach ($rows as $row)
